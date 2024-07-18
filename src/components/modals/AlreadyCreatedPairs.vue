9e3fc7b5170ff4571b27484e0cdb59fee04c0320
GeorgyGradinar <W2FbQC6BSedRjdc>
2024-07-18 10:02:57 +0400

    change delete loader in already created pairs

--- src/components/modals/AlreadyCreatedPairs.vue
+++ src/components/modals/AlreadyCreatedPairs.vue
@@ -7,17 +7,10 @@
   >
     <div class="content">
       <template v-if="!isAlreadySelectedPair">
-        <div class="pair" v-for="pair in alreadySelectedPair" :key="pair.params?.symbol">
-          <p>Пара: <span>{{ pair.params?.symbol }}</span></p>
-          <p>Количество монет: <span>{{ pair.params?.qty }}</span></p>
-          <p>Прайс покупки: <span>{{ pair.params?.price }}</span></p>
-          <p>Количество ордеров: <span>{{ pair.params?.qtyOpenOrders }}</span></p>
-          <p>Шаг в %: <span>{{ pair.params?.step }}</span></p>
-          <p>Decimals: <span>{{ pair.params?.decimals }}</span></p>
-
-          <v-btn class="primary-button reuse" @click="reusePair(pair)">Использовать</v-btn>
-          <v-btn class="primary-button delete" :loading="historyGridBot.isDeletePair.value" @click="deletePair(pair._id)">Удалить</v-btn>
-        </div>
+        <PairElement v-for="pair in alreadySelectedPair"
+                     :key="pair.params?.symbol"
+                     :pair="pair">
+        </PairElement>
       </template>
       <p v-if="isAlreadySelectedPair">Loading...</p>
       <p v-if="!alreadySelectedPair.length">У вас нет ранее созданых пар</p>
@@ -29,27 +22,13 @@
 import {modals} from "@/stores/modals";
 import {storeToRefs} from "pinia";
 import {pairs} from "@/stores/pairs";
-import gridBotHistory from "@/mixins/requests/bot/gridBotHistory";
+import PairElement from "@/components/modals/already-created-pairs/PairElement.vue";
 
 const pairsStore = pairs();
-const {changeSelectedPair} = pairsStore;
 const {alreadySelectedPair, isAlreadySelectedPair} = storeToRefs(pairsStore);
-const historyGridBot = gridBotHistory();
 
 const modalsStore = modals();
-const {toggleOpenAlreadyCreatedPair, toggleOpenAddPairModal} = modalsStore;
 const {isOpenAlreadyCreatedPairs} = storeToRefs(modalsStore);
-
-function reusePair(pair: any) {
-  changeSelectedPair(pair);
-  toggleOpenAlreadyCreatedPair(false);
-  toggleOpenAddPairModal(true);
-}
-
-async function deletePair(id: string): Promise<void> {
-  await historyGridBot.deletePair(id);
-  await historyGridBot.requestHistoryCreatedGridBots();
-}
 </script>
 
 <style scoped lang="scss">
@@ -67,41 +46,6 @@ async function deletePair(id: string): Promise<void> {
     background-color: var(--secondary-dark);
     border: 2px solid var(--dark-blue);
     overflow-y: auto;
-
-    .pair {
-      display: flex;
-      flex-direction: column;
-      gap: 10px;
-      max-width: 230px;
-      border: 2px solid var(--dark-blue);
-      padding: 20px;
-      transition: all 0.2s;
-
-      p {
-        span {
-          color: var(--primary-blue);
-        }
-      }
-
-      .reuse {
-        border: 1px solid var(--primary-blue);
-        --v-theme-surface: transparent;
-
-        &:hover {
-          background-color: var(--dark-blue);
-        }
-      }
-
-      .delete {
-        border: 1px solid var(--red);
-        color: var(--red);
-        --v-theme-surface: transparent;
-
-        &:hover {
-          background-color: var(--light-red);
-        }
-      }
-    }
   }
 }
 </style>
---
+++ src/components/modals/already-created-pairs/PairElement.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="pair">
+    <p>Пара: <span>{{ pair.params?.symbol }}</span></p>
+    <p>Количество монет: <span>{{ pair.params?.qty }}</span></p>
+    <p>Прайс покупки: <span>{{ pair.params?.price }}</span></p>
+    <p>Количество ордеров: <span>{{ pair.params?.qtyOpenOrders }}</span></p>
+    <p>Шаг в %: <span>{{ pair.params?.step }}</span></p>
+    <p>Decimals: <span>{{ pair.params?.decimals }}</span></p>
+
+    <v-btn class="primary-button reuse" @click="reusePair(pair)">Использовать</v-btn>
+    <v-btn class="primary-button delete"
+           @click="deletePair(pair._id)"
+           :loading="historyGridBot.isDeletePair.value">
+      Удалить
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {pairs} from "@/stores/pairs";
+import gridBotHistory from "@/mixins/requests/bot/gridBotHistory";
+import {modals} from "@/stores/modals";
+import {toRefs} from "vue";
+
+const props = defineProps({
+  pair: Object
+});
+const {pair} = toRefs(props);
+const pairsStore = pairs();
+const {changeSelectedPair} = pairsStore;
+const historyGridBot = gridBotHistory();
+
+const modalsStore = modals();
+const {toggleOpenAlreadyCreatedPair, toggleOpenAddPairModal} = modalsStore;
+
+function reusePair(pair: any) {
+  changeSelectedPair(pair);
+  toggleOpenAlreadyCreatedPair(false);
+  toggleOpenAddPairModal(true);
+}
+
+async function deletePair(id: string): Promise<void> {
+  await historyGridBot.deletePair(id);
+  await historyGridBot.requestHistoryCreatedGridBots();
+}
+</script>
+
+<style scoped lang="scss">
+@import "src/theme/buttons.css";
+
+.pair {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  max-width: 230px;
+  border: 2px solid var(--dark-blue);
+  padding: 20px;
+  transition: all 0.2s;
+
+  p {
+    span {
+      color: var(--primary-blue);
+    }
+  }
+
+  .reuse {
+    border: 1px solid var(--primary-blue);
+    --v-theme-surface: transparent;
+
+    &:hover {
+      background-color: var(--dark-blue);
+    }
+  }
+
+  .delete {
+    border: 1px solid var(--red);
+    color: var(--red);
+    --v-theme-surface: transparent;
+
+    &:hover {
+      background-color: var(--light-red);
+    }
+  }
+}
+</style>
