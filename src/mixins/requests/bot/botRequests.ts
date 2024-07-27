8ec10029c47fe4159b808a26cd4823b5b336acb6
ArtemDeminski <artem.d@konomic.com>
2024-07-27 13:07:07 +0400

    upd url

--- src/mixins/requests/bot/botRequests.ts
+++ src/mixins/requests/bot/botRequests.ts
@@ -45,7 +45,7 @@ export default function botRequests() {
   }
 
   function webSocketBotsInfo() {
-    const socket = new WebSocket('wss://dev.anturium.online/websocket');
+    const socket = new WebSocket('wss://anturium.online/websocket');
     socket.onopen = () => {
       socket.send(JSON.stringify({
         "authorization": personStore.token
