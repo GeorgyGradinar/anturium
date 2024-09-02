f511e9907244e9a00e742d3f335f482b5411b335
ArtemDeminski <artem.d@konomic.com>
2024-09-02 23:29:54 +0400

    add relistingBot to popup

---
+++ src/stores/popup.ts
@@ -0,0 +1,45 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+import {Popups} from "@/interfaces/sharedInterfaces";
+
+export const popupStore = defineStore('popupStore', () => {
+  const allPopups = ref<Popups[]>([]);
+
+  function addRejectPopup(text: string) {
+    const popup = {
+      id: getId(),
+      isDone: true,
+      text
+    };
+    allPopups.value.push(popup)
+    setTimeout(() => deletePopup(popup.id), 5000);
+  }
+
+  function addDonePopup(text: string) {
+    const popup = {
+      id: getId(),
+      isDone: true,
+      text
+    };
+    allPopups.value.push(popup)
+    setTimeout(() => deletePopup(popup.id), 5000);
+  }
+
+  function deletePopup(id: number) {
+    console.log(id)
+    const index = allPopups.value.findIndex((popup: Popups) => popup.id === id);
+
+    if (index === -1) return;
+
+    allPopups.value.splice(index, 1);
+  }
+
+  function getId() {
+    return Math.floor(Math.random() * 10000);
+  }
+
+  return {
+    addDonePopup,
+    addRejectPopup
+  }
+})
